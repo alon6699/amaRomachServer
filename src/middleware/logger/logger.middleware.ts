381ba7e31bb3061556370b27e6ebc24c304c987a
alon6699 <allonmaliii66@gmail.com>
2020-04-09 13:44:09 +0300

    renmae data to body

--- src/middleware/logger/logger.middleware.ts
+++ src/middleware/logger/logger.middleware.ts
@@ -5,7 +5,7 @@ export const loggerMiddleware = () => {
     return async (ctx: Context, next: Next) => {
         logger.info(`received request from url ${ctx.url}`);
         await next();
-        const message = `response status ${ctx.status}, data: ${JSON.stringify(ctx.body)}`;
+        const message = `response status ${ctx.status}, body: ${(JSON.stringify(ctx.body))}`;
         ctx.status >= 400 ? logger.error(message) : logger.info(message);
     };
 }
